a7d2a81d0c66f102e2211dba8c590b8c963fc3cd
krhamby <hambykr19@gcc.edu>
2022-10-19 14:41:04 -0400

    Comments and code to take input from an executable

--- GaleShapley.java
+++ GaleShapley.java
@@ -1,11 +1,9 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
-// import java.util.Random;
 
 /**
  * Class containing everything to execute the Gale-Shapley algorithm. Call
- * findStableMatches on your instance of this class. 
+ * findStableMatches on your instance of this class to find a set of stable matchings. 
  * Some ideaologies taken from the following:
  * https://www.sanfoundry.com/java-program-gale-shapley-algorithm/
  * @author Connor Felton, Kevin Hamby, Zachary O'Brien
@@ -20,6 +18,13 @@ class GaleShapley {
     private String[] womenPartners;
     private boolean[] isEngaged;
 
+    /**
+     * Constructor for GaleShapley class.
+     * @param listOfMen - list of names
+     * @param listOfWomen - list of names
+     * @param menPrefs - 2D array of preferences where each row is a list of the i man's preferences
+     * @param womenPrefs - 2D array of preferences where each row is a lits of the i woman's preferences
+     */
     public GaleShapley(String[] listOfMen, String[] listOfWomen, String[][] menPrefs, String[][] womenPrefs) {
         this.N = menPrefs.length;
         this.numEngaged = 0;
@@ -31,6 +36,9 @@ class GaleShapley {
         this.isEngaged = new boolean[N];
     }
 
+    /**
+     * Finds a set of stable matchings using the Gale-Shapley algorithm.
+     */
     public void findStableMatches() {
         while (numEngaged < N) {
 
@@ -60,7 +68,11 @@ class GaleShapley {
         // printPartners();
     }
 
-
+    /**
+     * Finds the index of a woman based on their name
+     * @param name - the name of the woman
+     * @return index of woman based on their name
+     */
     private int getWomenIndex(String name) {
         for (int i = 0; i < N; i++) {
             if (listOfWomen[i].equals(name)) {
@@ -70,6 +82,11 @@ class GaleShapley {
         return -1;
     }
 
+    /**
+     * Finds the index of a man based on their name
+     * @param name - the name of the man
+     * @return index of man based on their name
+     */
     private int getMenIndex(String name) {
         for (int i = 0; i < N; i++) {
             if (listOfMen[i].equals(name)) {
@@ -79,6 +96,13 @@ class GaleShapley {
         return -1;
     }
 
+    /**
+     * Returns a given woman's preference ranking (index) 
+     * for a given man
+     * @param index - the index value of the woman
+     * @param name - name of the man whose preference ranking is being found
+     * @return index representing the index (and therefore ranking) of the man
+     */
     private int getPreference(int index, String name) {
         for (int i = 0; i < womenPrefs[index][0].length(); i++) {
             if (womenPrefs[index][i].equals(name)) {
@@ -88,6 +112,9 @@ class GaleShapley {
         return -1;
     }
 
+    /**
+     * Prints out each partner. Only used for testing purposes.
+     */
     private void printPartners() {
         for (int i = 0; i < N; i++) {
             System.out.println("(" + womenPartners[i] + ", " + listOfWomen[i] + ")");
@@ -95,46 +122,42 @@ class GaleShapley {
     }
 
     public static void main(String[] args) {
-        // String[] m = {"M1", "M2", "M3", "M4", "M5"};
-        // /** list of women **/
-        // String[] w = {"W1", "W2", "W3", "W4", "W5"};
- 
-        // /** men preference **/
-        // String[][] mp = {{"W5", "W2", "W3", "W4", "W1"}, 
-        //                  {"W2", "W5", "W1", "W3", "W4"}, 
-        //                  {"W4", "W3", "W2", "W1", "W5"}, 
-        //                  {"W1", "W2", "W3", "W4", "W5"},
-        //                  {"W5", "W2", "W3", "W4", "W1"}};
-        // /** women preference **/                      
-        // String[][] wp = {{"M5", "M3", "M4", "M1", "M2"}, 
-        //                  {"M1", "M2", "M3", "M5", "M4"}, 
-        //                  {"M4", "M5", "M3", "M2", "M1"},
-        //                  {"M5", "M2", "M1", "M4", "M3"}, 
-        //                  {"M2", "M1", "M4", "M3", "M5"}};
+        // check that only 1 argument is passed
+        if (args.length != 1) {
+            System.out.println("Usage: java GaleShapley <size of random input>");
+            System.exit(1);
+        }
 
-        String[] m = new String[10000];
-        String[] w = new String[10000];
-        for (int i = 0; i < 10000; i++) {
+        // convert argument to int and catch error
+        int size = 0;
+        try {
+            size = Integer.parseInt(args[0]);
+        } catch (NumberFormatException e) {
+            System.out.println("Usage: java GaleShapley <size of random input>");
+            System.exit(1);
+        }
+
+        String[] m = new String[size];
+        String[] w = new String[size];
+        for (int i = 0; i < size; i++) {
             m[i] = "M" + (i + 1);
             w[i] = "W" + (i + 1);
         }
 
-        String[][] mp = new String[10000][10000];
-        String[][] wp = new String[10000][10000];
-        for (int i = 0; i < 10000; i++) {
-            String[] subMP = new String[10000];
-            String[] subWP = new String[10000];
+        String[][] mp = new String[size][size];
+        String[][] wp = new String[size][size];
+        for (int i = 0; i < size; i++) {
+            String[] subMP = new String[size];
+            String[] subWP = new String[size];
             ArrayList<Integer> randM = new ArrayList<Integer>();
             ArrayList<Integer> randW = new ArrayList<Integer>();
-            for (int k = 0; k < 10000; k++) {
+            for (int k = 0; k < size; k++) {
                 randM.add(k, k);
                 randW.add(k, k);
             }
             Collections.shuffle(randW);
             Collections.shuffle(randM);
-            // int[] randM = new Random().ints(1, 10000).distinct().limit(10000).toArray();
-            // int[] randW = new Random().ints(1, 10000).distinct().limit(10000).toArray();
-            for (int j = 0; j < 10000; j++) {
+            for (int j = 0; j < size; j++) {
                 subMP[j] = w[randM.get(j)];
                 subWP[j] = m[randW.get(j)];
             }
